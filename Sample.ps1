dd6edbd3288e04fcd0bfa4d3f901d4120e353395
Jason Li <jason.li@arinco.co.nz>
2025-04-11 02:15:31 +1200

    Improve pipeline

--- Sample.ps1
+++ Sample.ps1
@@ -10,8 +10,9 @@ try {
 
     ConvertTo-TMDL -tmslPath ".\Sales.bim" -outputPath ".\output\Sales"
     ConvertFrom-TMDL -tmdlPath ".\output\Sales" -outputPath ".\Sales_updated.bim" -databaseName "Sales"
-    Publish-TMDL -tmdlPath ".\output\Sales" -serverConnection "powerbi://api.powerbi.com/v1.0/myorg/capacity" -datasetName "Sales"
-    #Publish-TMDL -tmdlPath ".\Sales & Returns Sample v201912.SemanticModel\definition" -serverConnection "powerbi://api.powerbi.com/v1.0/myorg/capacity" -datasetName "Sales & Returns Sample"
+    #Publish-TMDL -tmdlPath ".\output\Sales" -serverConnection "powerbi://api.powerbi.com/v1.0/myorg/capacity" -datasetName "Sales"
+    Publish-TMDL -tmdlPath ".\Sales & Returns Sample v201912.SemanticModel\definition" -serverConnection "powerbi://api.powerbi.com/v1.0/myorg/capacity" `
+    -datasetName "Sales & Returns Sample"
 }
 catch {
     $ex = $_.Exception
